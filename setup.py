e78ae26d1d3571df7050b259cf65b57a2e41d7a3
apteksdi <apteksdi@gmail.com>
2022-11-21 13:07:08 +0700

    commit kedua

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        polkam=ckanext.polkam.plugin:POLKAMPlugin
+        polkam=ckanext.polkam.plugin:polkamplugin
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
